35f2afb1dab361b4b78596e8840c889eac159552
JaxNext <qianjunying@keep.com>
2024-10-30 15:58:28 +0800

    clean: remove unused toast

--- src/App.vue
+++ src/App.vue
@@ -6,5 +6,4 @@ import DarkMode from '@/components/DarkMode.vue'
   <div class="dark:bg-[rgb(2,8,23)] min-h-screen box-border p-6">
     <DarkMode class="fixed right-2 top-2" />
   </div>
-  <Toaster />
 </template>
